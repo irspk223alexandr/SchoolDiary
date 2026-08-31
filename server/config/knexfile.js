dc3ff075bc203786c4557b281c5854a9cf4bf57c
Ss1dan <sasha44rusrod@gmail.com>
2026-08-31 16:03:41 +0300

    db

--- server/config/knexfile.js
+++ server/config/knexfile.js
@@ -2,13 +2,19 @@ require('dotenv').config({ path: '../.env' });
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
+    connection: process.env.DATABASE_URL || {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
     },
+    migrations: { directory: '../migrations' },
+    seeds: { directory: '../seeds' }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: { directory: '../migrations' },
     seeds: { directory: '../seeds' }
   }
